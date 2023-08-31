a78930588cd5d75245acef24235410bbbccdfba9
bonbongiraffe <francescowai@yahoo.com>
2023-08-31 12:58:14 -0400

    columns to reservation forms

--- client/src/components/SeatSelection.js
+++ client/src/components/SeatSelection.js
@@ -18,14 +18,21 @@ function SeatSelection({ flight, user, setReservations }){
     }
 
     return (
-        <div className='mb-5'>
-            <h3 style={{color:'white'}}>Flight</h3>
-            <p style={{color:'white'}}>Origin: {flight.origin} - Destination: {flight.destination}</p>
-            <h3 style={{color:'white'}}>Seating Chart</h3>
-            {assets.seatingChartLegend}
-            <SeatChart openSeatslist={flight.open_seats} selectedSeat={selectedSeat} setSelectedSeat={setSelectedSeat}/>
-            {selectedSeat ? <p style={{color:'white'}}>Seat {selectedSeat} selected</p> : null}
-            {selectedSeat ? <button className='btn btn-primary' onClick={()=>handleSubmit()}>Reserve</button> : null}
+        <div className='mb-5 row'>
+            <div className='col-3' style={{width:'15rem'}}>
+                <h3 style={{color:'white'}}>Flight</h3>
+                <p style={{color:'white'}}>Origin: {flight.origin}</p>
+                <p style={{color:'white'}}>Destination: {flight.destination}</p>
+                <h3 style={{color:'white'}}>Seating Chart</h3>
+                {assets.seatingChartLegend}
+            </div>
+            <div className='col-6' style={{width:'25rem'}}>
+                <SeatChart openSeatslist={flight.open_seats} selectedSeat={selectedSeat} setSelectedSeat={setSelectedSeat}/>
+            </div>
+            <div className='col' style={{minWidth:'15rem'}}>
+                {selectedSeat ? <p style={{color:'white'}}>Seat {selectedSeat} selected</p> : null}
+                {selectedSeat ? <button className='btn btn-primary' onClick={()=>handleSubmit()}>Reserve</button> : null}
+            </div>
         </div>
     )
 }
