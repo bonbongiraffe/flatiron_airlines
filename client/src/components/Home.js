43b0bd541d7d879f56517d5a2af99df7dd57dbcf
bonbongiraffe <francescowai@yahoo.com>
2023-08-31 11:37:13 -0400

    resolve React terminal warnings, tidying code

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -78,7 +78,7 @@ function Home(){
           }, 5000); // Change photo every 5 seconds
     
         return () => clearInterval(interval);
-    },[])
+    },[photos.length])
 
     return( 
         <div id='home' className='container'>
@@ -94,7 +94,7 @@ function Home(){
             <div className='row'>
                 <div id='photos' className='card bg-light col m-2' style={{width:'37rem', height:'18rem'}}>
                     <div className='container' style={{position:'relative'}}>
-                        <img className='card-img-top mt-3' src={photos[photoIndex].path} alt='photo'/>
+                        <img className='card-img-top mt-3' src={photos[photoIndex].path} alt='travel-photos'/>
                         <div className='card-text' style={{backgroundColor:'#6C747C'}}>{dots}</div>
                     </div>
                     <div className='card-body'>
@@ -105,18 +105,18 @@ function Home(){
                 <div id='map' className='col container m-2' style={{position:'relative', backgroundColor:'rgb(10,163,207,0.7)', height:'23rem',borderRadius:'10px'}}>
                     <WorldMap style={{height:'25rem',width:'38rem',border:''}}/>
                     <div className='pin-container'>
-                        <img src={pin} className='pin' style={{top:'115px', left:'120px'}}/>
-                        <img src={pin} className='pin' style={{top:'100px', left:'135px'}}/>
-                        <img src={pin} className='pin' style={{top:'107px', left:'50px'}}/>
-                        <img src={pin} className='pin' style={{top:'85px', left:'55px'}}/>
-                        <img src={pin} className='pin' style={{top:'135px', left:'108px'}}/>
-                        <img src={pin} className='pin' style={{top:'110px', left:'75px'}}/>
-                        <img src={pin} className='pin' style={{top:'110px', left:'252px'}}/>
-                        <img src={pin} className='pin' style={{top:'98px', left:'275px'}}/>
-                        <img src={pin} className='pin' style={{top:'82px', left:'262px'}}/>
-                        <img src={pin} className='pin' style={{top:'145px', left:'480px'}}/>
-                        <img src={pin} className='pin' style={{top:'115px', left:'520px'}}/>
-                        <img src={pin} className='pin' style={{top:'118px', left:'499px'}}/>
+                        <img src={pin} alt='EWR' className='pin' style={{top:'115px', left:'120px'}}/>
+                        <img src={pin} alt='BOS' className='pin' style={{top:'100px', left:'135px'}}/>
+                        <img src={pin} alt='SFO' className='pin' style={{top:'107px', left:'50px'}}/>
+                        <img src={pin} alt='YVR' className='pin' style={{top:'85px', left:'55px'}}/>
+                        <img src={pin} alt='MIA' className='pin' style={{top:'135px', left:'108px'}}/>
+                        <img src={pin} alt='DEN' className='pin' style={{top:'110px', left:'75px'}}/>
+                        <img src={pin} alt='BCN' className='pin' style={{top:'110px', left:'252px'}}/>
+                        <img src={pin} alt='MUC' className='pin' style={{top:'98px', left:'275px'}}/>
+                        <img src={pin} alt='LHR' className='pin' style={{top:'82px', left:'262px'}}/>
+                        <img src={pin} alt='HKG' className='pin' style={{top:'145px', left:'480px'}}/>
+                        <img src={pin} alt='HND' className='pin' style={{top:'115px', left:'520px'}}/>
+                        <img src={pin} alt='ICN' className='pin' style={{top:'118px', left:'499px'}}/>
                     </div>
                 </div>
                 <div id='locations' className='card bg-light col-5 m-1' style={{width:'13rem', maxHeight:'25rem'}}>
